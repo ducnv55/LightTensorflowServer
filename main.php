2b959b7969e2a59845d0677c8e9f8de4cbe9ed5c
ducnv <nguyenvietduck55@gmail.com>
2017-10-26 07:53:10 +0000

    loop infinity

--- main.php
+++ main.php
@@ -1,4 +1,6 @@
 <?php
-shell_exec("php /var/www/html/camera/capture.php");
-shell_exec("python /var/www/html/image_processing/classify.py /var/www/html/test_dataset/captured_image.jpg > /var/www/html/result_analyzing/result.txt");
-print_r(shell_exec("/usr/bin/php /var/www/html/result_analyzing/ananlyze.php"));
+while (true) {
+	shell_exec("php /var/www/html/camera/capture.php");
+	shell_exec("python /var/www/html/image_processing/classify.py /var/www/html/test_dataset/captured_image.jpg > /var/www/html/result_analyzing/result.txt");
+	print_r(shell_exec("/usr/bin/php /var/www/html/result_analyzing/ananlyze.php"));
+}
--- result_analyzing/ananlyze.php
+++ result_analyzing/ananlyze.php
@@ -8,7 +8,7 @@ for ($i = 0; $i < count($lines); $i+=2) {
 file_put_contents('/var/www/html/result_analyzing/result_json.txt', json_encode($output));
 foreach ($output as $key => $value) {
 	print_r($key . ": " . $value*100 . "%\n");
-	if ((string)$key == "dog" && (int)($value*100) > 90) {
+	if ((string)$key == "dog" && (int)($value*100) > 80 || (string)$key == "minh" && (int)($value*100) > 70) {
 		print_r($key . " is detected\n");
 		shell_exec("php /var/www/html/raspberry_pin/switch.php");
 	}
